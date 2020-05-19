c777600df4bc7b23d8cf29cb6739731ab5cdc799
Nilesh Kulkarni <meet2nilesh.gad@gmail.com>
2020-05-19 17:35:09 +0530

    fimal

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowCategoriesHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -98,11 +98,12 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
-
+      var chosenCategoryShortName = chosenCategoryShortName(categories);
+      //insertHtml("#main-content", chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -115,9 +116,24 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage += "<section class='row'>";
+        // Loop over categories
+        for (var i = 0; i < homeHtmlToInsertIntoMainPage.length; i++) {
+          // Insert category values
+          var html = categoryHtml;
+          var name = "" + categories[i].name;
+          var short_name = categories[i].short_name;
+          html =
+            insertProperty(html, "name", name);
+          html =
+            insertProperty(html,
+                           "short_name",
+                           short_name);
+          homeHtmlToInsertIntoMainPage  += html;
+        }
 
-
+        homeHtmlToInsertIntoMainPage  += "</section>";
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
